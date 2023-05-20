6abc618051631ef6d364a87fa32cb63b09f241e3
webader <alexey.derenko@gmail.com>
2023-05-20 09:13:11 +0300

    base_ts

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
 );
+root.render(
+    <App/>
+);
+
