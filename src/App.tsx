89e6638a810ddb31de3b806c56e12cc0fb3c7c77
webader <alexey.derenko@gmail.com>
2023-06-25 19:05:51 +0300

    lesson8_ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import {Navigate, Route, Routes} from 'react-router-dom';
+
+import {MainLayout} from './layouts';
+import {CarPage} from './pages';
 
 const App = () => {
-  return (
-      <div>
-        App
-      </div>
-  );
+    return (
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+                <Route index element={<Navigate to={'cars'}/>}/>
+                <Route path={'cars'} element={<CarPage/>}/>
+            </Route>
+        </Routes>
+    );
 };
 
 export default App;
